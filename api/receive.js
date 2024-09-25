56aa9ffd8c20eebb29e6ccce7c2c9628e47f1b6e
CX330Blake <timchen975@gmail.com>
2024-09-25 13:01:33 +0800

    .

---
+++ api/receive.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+    if (req.method === "GET") {
+        const cookie = req.query.cookie; // 从查询参数获取 cookie
+        if (cookie) {
+            console.log("Received cookie:", cookie); // 打印到服务器日志
+        }
+        res.status(200).send("Cookie received");
+    } else {
+        res.status(405).send("Method not allowed");
+    }
+}
